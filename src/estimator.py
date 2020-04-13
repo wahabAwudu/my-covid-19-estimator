b3ce0ad1ded9d2aeae08da83e299dabd702a7d2e
wahabawudu <wahabawudu2020@gmail.com>
2020-04-13 18:07:21 +0000

    finish estimator assignments

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,70 @@
+import math
+
+
+def getDurationDays(data):
+  '''convert weeks and months to days'''
+  
+  if data['periodType'] == 'weeks':
+    return data['timeToElapse'] * 7
+  elif data['periodType'] == 'months':
+    return data['timeToElapse'] * 30
+  return data['timeToElapse']
+
+
+def impactEstimator(data):
+  '''estimates the imapact using the data provided'''
+
+  currentlyInfected = data['reportedCases'] * 10
+  exp = getDurationDays(data) / 3
+  infectionsByRequestedTime = currentlyInfected * math.ceil((2**exp))
+  severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
+  hospitalBedsByRequestedTime = 0.35 * data['totalHospitalBeds']
+  casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
+  casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
+  dollarsInFlight = (infectionsByRequestedTime * data['region']['avgDailyIncomePopulation'])\
+    * data['avgDailyIncomeInUSD'] * getDurationDays(data)
+
+  return {
+    'currentlyInfected': currentlyInfected,
+    'infectionsByRequestedTime': infectionsByRequestedTime,
+    'severeCasesByRequestedTime': severeCasesByRequestedTime,
+    'hospitalBedsByRequestedTime': hospitalBedsByRequestedTime,
+    'casesForICUByRequestedTime': casesForICUByRequestedTime,
+    'casesForVentilatorsByRequestedTime': casesForVentilatorsByRequestedTime,
+    'dollarsInFlight': dollarsInFlight
+  }
+
+
+def severeImactEstimator(data):
+  '''estimates the severe impact using the data provided'''
+
+  currentlyInfected = data['reportedCases'] * 50
+  exp = getDurationDays(data) / 3
+  infectionsByRequestedTime = currentlyInfected * (2**exp)
+  severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
+  hospitalBedsByRequestedTime = 0.35 * data['totalHospitalBeds']
+  casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
+  casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
+  dollarsInFlight = (infectionsByRequestedTime * data['region']['avgDailyIncomePopulation'])\
+    * data['avgDailyIncomeInUSD'] * getDurationDays(data)
+
+  return {
+    'currentlyInfected': currentlyInfected,
+    'infectionsByRequestedTime': infectionsByRequestedTime,
+    'severeCasesByRequestedTime': severeCasesByRequestedTime,
+    'hospitalBedsByRequestedTime': hospitalBedsByRequestedTime,
+    'casesForICUByRequestedTime': casesForICUByRequestedTime,
+    'casesForVentilatorsByRequestedTime': casesForVentilatorsByRequestedTime,
+    'dollarsInFlight': dollarsInFlight
+  }
+
+
 def estimator(data):
-  return data
+  '''collates all data for estimate functions'''
+
+  estimate = {
+    'data': data,
+    'impact': impactEstimator(data),
+    'severeImpact': severeImactEstimator(data)
+  }
+  return estimate
